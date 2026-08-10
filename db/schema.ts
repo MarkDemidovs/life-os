8242b225bccec6222bd1fdd002c09af061436968
MarkDemidovs <markdemidovs@proton.me>
2026-08-10 23:33:17 +0300

    tasks schema

---
+++ db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, serial, text, boolean } from "drizzle-orm/pg-core";
+
+export const tasks = pgTable("tasks", {
+    id: serial("id").primaryKey(),
+    taskName: text("task_name").notNull().default("New Todo"),
+    isCompleted: boolean("is_completed").notNull().default(false),
+    userId: text("user_id").notNull()
+})
+
+export type Task = typeof tasks.$inferSelect;

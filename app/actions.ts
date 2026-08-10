e8e6c55e41773b3033ea3614296fe0aed20b88d1
MarkDemidovs <markdemidovs@proton.me>
2026-08-10 23:47:28 +0300

    createTask

--- app/actions.ts
+++ app/actions.ts
@@ -6,7 +6,25 @@ import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 
 export async function getTasks() {
-    const { userId } = await auth();
+    const { userId } = await auth.protect();
 
     return await db.select().from(tasks).where(eq(tasks.userId, userId));
+}
+
+export async function createTask(formData: FormData) {
+    const { userId } = await auth.protect();
+
+    const result = taskSchema.safeParse({
+        taskName: formData.get("taskName")
+    });
+
+    if (!result.success) {
+        console.error(result.error);
+        throw new Error("Invalid task data");
+    }
+
+    await db.insert(tasks).values({
+        taskName: result.data.taskName,
+        userId
+    })
 }

61f1d84f9a9b440fed2a396baa204a96985aead7
MarkDemidovs <markdemidovs@proton.me>
2026-08-14 01:26:38 +0300

    optimistic updates

--- app/tasks/components/Task.tsx
+++ app/tasks/components/Task.tsx
@@ -3,7 +3,7 @@ import TaskCheckbox from "./TaskCheckbox";
 export default function Task({ id, title, status }: { id: number, title: string, status: boolean }) {
     return (
         <div>
-            <p>{title}</p>
+            <p className={status ? "text-gray-400 line-through" : "text-gray-100 "}>{title}</p>
             <TaskCheckbox id={id} status={status}/>
         </div>
     );

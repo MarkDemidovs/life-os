1c6a36bd652819034d60241bf81ad1363c98101a
MarkDemidovs <markdemidovs@proton.me>
2026-08-25 22:42:29 +0300

    design for delete

--- app/tasks/components/Task.tsx
+++ app/tasks/components/Task.tsx
@@ -1,11 +1,13 @@
 import TaskCheckbox from "./TaskCheckbox";
+import TaskDelete from "./TaskDelete";
 
 export default function Task({ id, title, status }: { id: number, title: string, status: boolean }) {
     return (
-        <div className="flex w-72 items-center justify-between py-2 px-3 hover:bg-gray-800/50 rounded-lg">
+        <div className="flex w-96 items-center justify-between py-2 px-3 hover:bg-gray-800/50 rounded-lg">
             <p className={status ? "text-gray-400 line-through" : "text-gray-100 "}>{title}</p>
             <div className="pr-2">
                 <TaskCheckbox id={id} status={status} />
+                <TaskDelete />
             </div>
         </div>
     );
---
+++ app/tasks/components/TaskDelete.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { deleteTask } from "@/app/actions";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { TaskType } from "@/db/schema";
+
+export default function TaskDelete() {
+    return (
+        <button className="ml-10">
+            Delete
+        </button>
+    )
+}

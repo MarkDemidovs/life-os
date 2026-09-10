1b2f7e44f7e7641a06d0012ee4d3ec755ae7c97d
Mark Demidovs <markdemidovs@proton.me>
2026-09-10 10:50:32 +0300

    initialHabits

--- app/habits/page.tsx
+++ app/habits/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
 import HomeButton from "@/components/ui/homebutton";
+import { habits } from "@/db/schema";
+import { db } from "@/db";
 export default async function Habits() {
   await auth.protect();
+  const initialHabits = await db.select().from(habits).orderBy(asc(habits.id));
 
   return (
     <>
